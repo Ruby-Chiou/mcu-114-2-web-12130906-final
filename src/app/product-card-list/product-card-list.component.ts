f46f80fc49a9cf0929b15dfdf3743d50915636d3
Ruby Choiu <12130906@me.mcu.edu.tw>
2026-06-17 07:29:26 +0800

    feat(product): 建立產品資料模型

--- src/app/product-card-list/product-card-list.component.ts
+++ src/app/product-card-list/product-card-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProductCardComponent } from '../product-card/product-card.component';
+import { Product } from '../model/product';
 
 @Component({
   selector: 'app-product-card-list',
@@ -8,10 +9,11 @@ import { ProductCardComponent } from '../product-card/product-card.component';
   styleUrl: './product-card-list.component.scss',
 })
 export class ProductCardListComponent {
-  protected productName = 'A 書籍';
-  protected author = '作者 A、作者 B、作者 C';
-  protected company = '博碩文化';
-  protected price = 1580;
-
-  protected photoUrl = 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img';
+  product = new Product({
+    name: 'A 書籍',
+    authors: '作者 A、作者 B、作者 C',
+    company: '博碩文化',
+    price: 1580,
+    photoUrl: 'https://api.fnkr.net/testimg/200x200/DDDDDD/999999/?text=img',
+  });
 }
