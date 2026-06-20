88886b683d373d7d114c41fbc1efc82ef136db29
Ruby Choiu <12130906@me.mcu.edu.tw>
2026-06-20 08:35:00 +0800

    feat(product-detail): 新增產品檢視頁面內容

--- src/app/product-detail-page/product-detail-page.component.ts
+++ src/app/product-detail-page/product-detail-page.component.ts
@@ -1,9 +1,34 @@
-import { Component } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, input, numberAttribute, OnInit, signal } from '@angular/core';
+import { Product } from '../model/product';
+import { Router } from '@angular/router';
+import { ProductService } from '../services/product.service';
 
 @Component({
   selector: 'app-product-detail-page',
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-detail-page.component.html',
   styleUrl: './product-detail-page.component.scss',
 })
-export class ProductDetailPageComponent {}
+export class ProductDetailPageComponent implements OnInit {
+  id = input.required<number, string | number>({ transform: numberAttribute });
+
+  protected readonly product = signal<Product>(new Product());
+
+  private readonly router = inject(Router);
+
+  private readonly productService = inject(ProductService);
+
+  ngOnInit(): void {
+    const product = this.productService.getById(this.id());
+    this.product.set(product);
+  }
+
+  /*onEdit(): void {
+    this.router.navigate(['product', 'form', this.product().id]);
+  }*/
+
+  onBack(): void {
+    this.router.navigate(['products']);
+  }
+}
