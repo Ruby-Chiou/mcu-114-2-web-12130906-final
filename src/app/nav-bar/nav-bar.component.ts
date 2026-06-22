7fdfba2bfac0fa0c2614c3fdc42c85e019b46f06
Ruby Choiu <12130906@me.mcu.edu.tw>
2026-06-23 04:32:26 +0800

    feat(nav-bar): 變更購物車數量樣式

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -12,7 +12,7 @@ export class NavBarComponent {
   protected readonly router = inject(Router);
   protected readonly cartService = inject(CartService);
 
-  // ⭕ 3. 自動計算購物車內所有商品的「數量總和」
+  //自動計算購物車內所有商品的「數量總和」
   protected readonly cartCount = computed(() => {
     return this.cartService.cartItems().reduce((sum, item) => sum + item.quantity, 0);
   });
